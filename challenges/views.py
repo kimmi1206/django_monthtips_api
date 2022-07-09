521ac70585f49af1523040707ed53ae30f69352a
kimmi1206 <kheinerimm1206@gmail.com>
2022-07-08 21:20:57 -0500

    added cdynamic paths

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
 
+months = {'january': "january data",
+          'february': "february data",
+          'march': "march data",
+          'april': "april data",
+          'may': "may data",
+          'june': "june data",
+          'july': "july data",
+          'august': "august data",
+          'september': "september data",
+          'october': "october data",
+          'november': "november data",
+          'december': "december data", }
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month in months:
+        challenge_text = months[month]
+    else:
+        challenge_text = f"Month *{month}* is Not supported"
+    return HttpResponse(challenge_text)
--- djangoblogapi/settings.py
+++ djangoblogapi/settings.py
@@ -11,15 +11,26 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Add .env variables anywhere before SECRET_KEY
+load_dotenv()
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
