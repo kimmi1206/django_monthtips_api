7b20016caf0b226d13e9682bc302428643091f90
kimmi1206 <kheinerimm1206@gmail.com>
2022-07-09 20:12:46 -0500

    Added Challenge Template

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.urls import reverse
+from django.template.loader import render_to_string
+
 
 months = {'january': "january data",
           'february': "february data",
@@ -13,13 +16,43 @@ months = {'january': "january data",
           'october': "october data",
           'november': "november data",
           'december': "december data", }
+
+
 # Create your views here.
 
 
+def index(request):
+    list_items = ""
+    months_list = list(months.keys())
+
+    for month in months_list:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<h1><ul>{list_items}</ul></h1>"
+
+    return HttpResponse(response_data)
+
+
+def monthly_challenge_by_number(request, month):
+    months_list = list(months.keys())
+
+    if month < 1 or month > len(months_list):
+        return HttpResponseNotFound(f"<h2>Month *{month}* is Not supported</h2>")
+
+    redirect_month = months_list[int(month) - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month in months:
+    try:
+        # if month in months:
         challenge_text = months[month]
-    else:
-        challenge_text = f"Month *{month}* is Not supported"
-    return HttpResponse(challenge_text)
+        return render(request, "challenges/challenge.html", {
+            "text": challenge_text,
+            "item": month.capitalize(),
+        })
+    except KeyError:
+        return HttpResponseNotFound(f"<h2>Month *{month}* is Not supported</h2>")
--- djangoblogapi/settings.py
+++ djangoblogapi/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'challenges',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
